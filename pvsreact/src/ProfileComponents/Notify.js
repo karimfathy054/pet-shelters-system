d6f4415bca6415db5da125fb05e3568259c3af0c
OmarMahmoud11 <akatskiamv65@gmail.com>
2023-12-30 09:33:49 +0200

    Pets Changes final233

--- pvsreact/src/ProfileComponents/Notify.js
+++ pvsreact/src/ProfileComponents/Notify.js
@@ -2,6 +2,27 @@ import styles from "../CSS/profileStyles.module.css";
 import { useState, useEffect } from "react";
 export default function Notify({ user }) {
     const [notifies, setNotifies] = useState([]);
+    const [temp, setTemp] = useState(true);
+    useEffect(() => {
+        if (temp) {
+            fetch(`http://localhost:8080/adopter/getNotification/${user.adopterId}`, {
+                method: 'GET',
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (!data.status) {
+                        setNotifies(data);
+                    }
+                    console.log(data);
+                    setTemp(false);
+                })
+                .catch(error => {
+                    console.error('Error creating user:', error);
+                    setTemp(false);
+                });
+        }
+    }
+    )
     const handleReed = () => {
 
     }
@@ -13,32 +34,19 @@ export default function Notify({ user }) {
                         {notifies.map((product, index) => {
                             return (
                                 (<div class={styles.box}>
-                                    {product.imageLink ? (<div class={styles.image}><img src={require("../images/" + product.imageLink)} alt="" /></div>) : (<></>)}
                                     <div class={styles.text}>
-                                        <h3>{product.productName}</h3>
-                                        <p>{product.description}</p>
+                                        <h3>Name</h3>
+                                        <p>description</p>
                                     </div>
-                                    <div className={styles.brand}><p>Type</p>{product.type}</div>
-                                    <div className={styles.brand}><p>Birth Date</p>{product.birthDate}</div>
-                                    <div className={styles.category}><p>Breed</p>{product.breed}</div>
+                                    <div className={styles.brand}><p>Status</p>Status</div>
                                     <div className={styles.actionProduct}>
-                                        <div className={styles.accept} onClick={handleAcceptPet} id={index}>Accept</div>
-                                        <div className={styles.refuse} onClick={handleRefusePet} id={index}>Refuse</div>
+                                        <div className={styles.accept} onClick={handleReed} id="">Done</div>
                                     </div>
                                 </div>
                                 )
                             )
                         })}
-                        <div class={styles.box}>
-                            <div class={styles.text}>
-                                <h3>Name</h3>
-                                <p>description</p>
-                            </div>
-                            <div className={styles.brand}><p>Status</p>Status</div>
-                            <div className={styles.actionProduct}>
-                                <div className={styles.accept} onClick={handleReed} id="">Done</div>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
